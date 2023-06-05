074905e6c57c5ecadffdd86f27200ea78a7b4313
scarreira-dg <stephane.scarreira@gmail.com>
2023-06-05 09:39:16 +0100

    multiple bug fixes, now saves per page upon restart

--- preload.js
+++ preload.js
@@ -1,36 +1,48 @@
-const { contextBridge } = require("electron");
+const {
+    contextBridge
+} = require("electron");
 const itemsMgr = require("./src/models/items");
 const settings = require("./src/js/settings");
 
 const getAll = (searchQuery = "", limit = 100, offset = 0) => {
-  return itemsMgr.getAll(searchQuery, limit, offset);
+    return itemsMgr.getAll(searchQuery, limit, offset);
 };
 
 const getCategories = () => {
-  return itemsMgr.getCategories();
+    return itemsMgr.getCategories();
 };
 
 const filterByCategory = (searchQuery = "", category = null, limit = 100, offset = 0) => {
-  return itemsMgr.filterByCategory(searchQuery, category, limit, offset);
+    return itemsMgr.filterByCategory(searchQuery, category, limit, offset);
 };
 
 const totalPages = (limit = 100, category = "null", searchQuery = "") => {
-  return itemsMgr.totalPages(limit, category, searchQuery);
+    return itemsMgr.totalPages(limit, category, searchQuery);
 };
 
 const setDatabasePath = (path) => {
-  return settings.setDatabasePath(path);
+    return settings.setDatabasePath(path);
 };
 
 const getDatabasePath = () => {
-  return settings.getDatabasePath();
+    return settings.getDatabasePath();
+};
+
+const setPerPage = (perpage) => {
+    return settings.setPerPage(perpage);
+};
+
+const getPerPage = () => {
+    return settings.getPerPage();
 };
 
 contextBridge.exposeInMainWorld("electron", {
-  totalPages: totalPages,
-  getAll: getAll,
-  getCategories: getCategories,
-  filterByCategory: filterByCategory,
-  setDatabasePath: setDatabasePath,
-  getDatabasePath: getDatabasePath,
+    totalPages: totalPages,
+    getAll: getAll,
+    getCategories: getCategories,
+    filterByCategory: filterByCategory,
+    setDatabasePath: setDatabasePath,
+    getDatabasePath: getDatabasePath,
+    setPerPage: setPerPage,
+    getPerPage: getPerPage
 });
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,200 +1,207 @@
 var items, currentPage, totalPages, category, searchQuery, perPage, tbody;
 
 document.addEventListener("DOMContentLoaded", async () => {
-  if (!window.electron.getDatabasePath()) {
-    window.location.href = "../settings.html";
-  }
+    if (!window.electron.getDatabasePath()) {
+        //window.location.href = "../settings.html";
 
-  loading(true);
-  try {
-    items = window.electron.getAll();
-  } catch {
-    alert("Database missing, or corrupt. Please choose a valid database.");
-    return;
-  } finally {
-    loading(false);
-  }
-
-  currentPage = 1;
-  searchQuery = "";
-
-  totalPages = window.electron.totalPages();
-  category = "null";
-
-  tbody = document.getElementById("items");
-  perPage = $("#perpage").val();
-
-  initPagination(totalPages);
-
-  setRecords(items);
-
-  let categories = window.electron.getCategories();
-
-  categories.forEach((category) => {
-    $("#category").append(
-      $("<option>", {
-        value: category.cat,
-        text: snakeToPascal(category.cat),
-      })
-    );
-  });
-
-  $("#category").val(category);
-
-  $("#category").on("change", function () {
-    loading(true);
-    loadCategory(this.value);
-    loading(false);
-  });
-
-  $("#perpage").on("change", async function () {
-    loading(true);
-    perPage = $("#perpage").val();
-    items = window.electron.filterByCategory(searchQuery, category, $("#perpage").val(), currentPage * perPage - perPage);
-    setRecords(items);
-    loading(false);
-  });
-
-  $("#search").on("keypress", function (e) {
-    if (e.which == 13) {
-      searchByQuery($(this).val());
     }
-  });
+
+    loading(true);
+    try {
+        items = window.electron.getAll();
+    } catch {
+        alert("Database missing, or corrupt. Please choose a valid database.");
+        return;
+    } finally {
+        loading(false);
+    }
+
+    currentPage = 1;
+    searchQuery = "";
+
+    category = "null";
+
+    tbody = document.getElementById("items");
+    perPage = window.electron.getPerPage();
+    $("#perpage").val(perPage);
+    totalPages = window.electron.totalPages(perPage);
+
+
+    initPagination(totalPages);
+
+    setRecords(items);
+
+    let categories = window.electron.getCategories();
+
+    categories.forEach((category) => {
+        $("#category").append(
+            $("<option>", {
+                value: category.cat,
+                text: snakeToPascal(category.cat),
+            })
+        );
+    });
+
+    $("#category").val(category);
+
+    $("#category").on("change", function() {
+        loading(true);
+        category = this.value;
+        loadCategory(this.value);
+        loading(false);
+    });
+
+    $("#perpage").on("change", async function() {
+        loading(true);
+        perPage = $("#perpage").val();
+        window.electron.setPerPage(perPage);
+        initPagination(window.electron.totalPages(perPage, category, searchQuery));
+        items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
+        setRecords(items);
+        loading(false);
+    });
+
+    $("#search").on("keypress", function(e) {
+        if (e.which == 13) {
+            searchQuery = $(this).val();
+            searchByQuery(searchQuery, category, perPage, currentPage * perPage - perPage);
+        }
+    });
 });
 
 function loading(start = false) {
-  if (start) {
-    $("#loading").removeClass("d-none");
-    $("#loading").addClass("d-block");
+    if (start) {
+        $("#loading").removeClass("d-none");
+        $("#loading").addClass("d-block");
+        return;
+    }
+    $("#loading").removeClass("d-block");
+    $("#loading").addClass("d-none");
     return;
-  }
-  $("#loading").removeClass("d-block");
-  $("#loading").addClass("d-none");
-  return;
 }
 
 function unsetActive() {
-  $(`.page-link`).each(function (index, element) {
-    $(this).removeClass("active");
-  });
+    $(`.page-link`).each(function(index, element) {
+        $(this).removeClass("active");
+    });
 }
 
 const snakeToPascal = (string) => {
-  return string
-    .split("/")
-    .map((snake) =>
-      snake
-        .split("_")
-        .map((substr) => substr.charAt(0).toUpperCase() + substr.slice(1))
-        .join("")
-    )
-    .join("/");
+    return string
+        .split("/")
+        .map((snake) =>
+            snake
+            .split("_")
+            .map((substr) => substr.charAt(0).toUpperCase() + substr.slice(1))
+            .join("")
+        )
+        .join("/");
 };
 
 function setCurrentPage(value) {
-  if (value.trim() == "...") {
-    return currentPage;
-  }
-  if (value.trim() == "Next") {
-    return currentPage + 1 < totalPages ? currentPage + 1 : totalPages;
-  } else if (value.trim() == `Prev`) {
-    return currentPage - 1 > 0 ? currentPage - 1 : 1;
-  }
-  return parseInt(value);
+    if (value.trim() == "...") {
+        return currentPage;
+    }
+    if (value.trim() == "Next") {
+        return currentPage + 1 < totalPages ? currentPage + 1 : totalPages;
+    } else if (value.trim() == `Prev`) {
+        return currentPage - 1 > 0 ? currentPage - 1 : 1;
+    }
+    return parseInt(value);
 }
 
-function searchByQuery(searchQuery) {
-  loading(true);
-  currentPage = 1;
-  items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
-  totalPages = window.electron.totalPages(perPage, category, searchQuery);
-  initPagination(totalPages);
-  setRecords(items);
-  loading(false);
+function searchByQuery(searchQuery, category, limit, offset) {
+    loading(true);
+    currentPage = 1;
+    items = window.electron.filterByCategory(searchQuery, category, limit, offset);
+    totalPages = window.electron.totalPages(perPage, category, searchQuery);
+    initPagination(totalPages);
+    setRecords(items);
+    loading(false);
 }
 
 function loadCategory(category) {
-  category = category;
-  currentPage = 1;
-  items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
+    category = category;
+    currentPage = 1;
+    items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
 
-  totalPages = window.electron.totalPages(perPage, category, searchQuery);
-  initPagination(totalPages);
-  setRecords(items);
+    totalPages = window.electron.totalPages(perPage, category, searchQuery);
+    initPagination(totalPages);
+    setRecords(items);
 }
 
 function initPagination(totalPages) {
-  var dfrd1 = $.Deferred();
+    var dfrd1 = $.Deferred();
 
-  let html = `<li class="page-item">
+    let html = `<li class="page-item">
                   <a class="page-link" href="#" aria-label="Previous">
                     Prev
                   </a>
                 </li>`;
 
-  for (let j = 1; j <= 3; j++) {
-    if (j <= totalPages) {
-      html += `
+    for (let j = 1; j <= 3; j++) {
+        if (j <= totalPages) {
+            html += `
           <li class="page-item"><a id="page${j}" class="page-link" href="#">${j}</a></li>`;
+        }
     }
-  }
 
-  if (currentPage > 7) {
-    html += ` <li class="page-item"><a class="page-link" href="#">...</a></li>`;
-  }
-  $("#pages").html(html);
-  let displayPages;
-  if (totalPages >= 3) {
-    displayPages = currentPage + 6 >= totalPages ? totalPages - 3 : currentPage + 3;
-  } else {
-    displayPages = totalPages - 3;
-  }
-  let i = currentPage > 6 ? currentPage - 3 : 4;
+    if (currentPage > 7) {
+        html += ` <li class="page-item"><a class="page-link" href="#">...</a></li>`;
+    }
+    $("#pages").html(html);
+    let displayPages;
+    if (totalPages >= 3) {
+        displayPages = currentPage + 6 >= totalPages ? totalPages - 3 : currentPage + 3;
+    } else {
+        displayPages = totalPages - 3;
+    }
+    let i = currentPage > 6 ? currentPage - 3 : 4;
 
-  for (i; i <= displayPages; i++) {
-    html += `<li class="page-item"><a id="page${i}" class="page-link" href="#">${i}</a></li>`;
-  }
+    for (i; i <= displayPages; i++) {
+        html += `<li class="page-item"><a id="page${i}" class="page-link" href="#">${i}</a></li>`;
+    }
 
-  if (currentPage <= totalPages - 7) {
-    html += ` <li class="page-item"><a class="page-link" href="#">...</a></li>`;
-  }
+    if (currentPage <= totalPages - 7) {
+        html += ` <li class="page-item"><a class="page-link" href="#">...</a></li>`;
+    }
 
-  if (totalPages > 10) {
-    html += `
+    if (totalPages > 10) {
+        html += `
     <li class="page-item"><a id="page${totalPages - 2}" class="page-link" href="#">${totalPages - 2}</a></li>
     <li class="page-item"><a id="page${totalPages - 1}" class="page-link" href="#">${totalPages - 1}</a></li>
     <li class="page-item"><a id="page${totalPages}" class="page-link" href="#" >${totalPages}</a></li>`;
-  }
+    }
 
-  html += ` <li class="page-item">
+    html += ` <li class="page-item">
                 <a class="page-link" href="#" aria-label="Next">
                   Next
                 </a>
               </li>`;
 
-  $(".pagination").html(html);
-  $(`#page${currentPage}`).addClass("active");
-
-  $(".page-link").on("click", function (e) {
-    e.preventDefault();
-    loading(true);
-    unsetActive();
-    currentPage = setCurrentPage($(this).text());
-    items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
-    initPagination(totalPages);
+    $(".pagination").html(html);
     $(`#page${currentPage}`).addClass("active");
 
-    setRecords(items);
-    loading(false);
-  });
+    $(".page-link").on("click", function(e) {
+        e.preventDefault();
+        loading(true);
+        unsetActive();
+        currentPage = setCurrentPage($(this).text());
+        items = window.electron.filterByCategory(searchQuery, category, perPage, currentPage * perPage - perPage);
+        initPagination(totalPages);
+        $(`#page${currentPage}`).addClass("active");
 
-  return dfrd1.promise();
+        setRecords(items);
+        loading(false);
+    });
+
+    return dfrd1.promise();
 }
 
 function setRecords(rows) {
-  tbody.innerHTML = "";
-  rows.forEach((row) => {
-    tbody.innerHTML += `<tr>
+    tbody.innerHTML = "";
+    rows.forEach((row) => {
+        tbody.innerHTML += `<tr>
                   <td>${row.id}</td>
                   <td style="text-align: left;">${row.title}</td>
                   <td>${row.cat}</td>
@@ -202,5 +209,5 @@ function setRecords(rows) {
                   <td>${(row.size / 1000000000).toFixed(3)} GB</td>
                   <td ><a class="text-danger" href="magnet:?xt=urn:btih:${row.hash}"><i class="bi bi-magnet-fill"></i></a></td>
                 </tr>`;
-  });
+    });
 }
